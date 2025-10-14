6fea9728de68bc2495b141f72cd12237dbfb1b85
Shubham Awasthi <shubhamawasthi0403@gmail.com>
2025-10-15 00:15:12 +0530

    first commit

---
+++ app/(main)/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1>
+            Welcome to SpeakSmart AI
+        </h1>
+        <Button>
+            Welcome
+        </Button>
+    </div>
+  )
+}
+
+export default HomePage
